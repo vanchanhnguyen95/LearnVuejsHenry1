9800f8f9c8bcd67a664f81bccba9410e6c7da21e
DESKTOP-T0HLH7K\os <vanchanhnguyen95@gmail.com>
2022-06-01 23:33:42 +0700

    upd

---
+++ tap1/src/components/Header.vue
@@ -0,0 +1,17 @@
+<template>
+    <header class="header">
+        <h1>Việc cần làm</h1>
+    </header>
+</template>
+<script>
+export default {
+    name:'Header'
+}
+</script>
+<style scoped>
+    .header {
+        background: #333;
+        text-align: center;
+        padding: 10px;
+    }
+</style>
