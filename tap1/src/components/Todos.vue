e2322db5ffba99d7457aa81644b5a32fc2e5a6f1
DESKTOP-T0HLH7K\os <vanchanhnguyen95@gmail.com>
2022-06-03 00:17:41 +0700

    add Todos.vue

---
+++ tap1/src/components/Todos.vue
@@ -0,0 +1,19 @@
+<template>
+<div class="todos">
+    <p>Việc 1</p>
+    <p>Việc 2</p>
+    <p>Việc 3</p>
+</div>
+    
+</template>
+<script>
+export default {
+    name: 'Todos'
+}
+</script>
+
+<style scoped>
+    .todos{
+        text-align: left;
+    }
+</style>
